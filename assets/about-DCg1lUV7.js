b8e7d29cd7a2e0a279bbc9a318ce51cb73839a16
Deniz Satır <deniznehlyadyuk@gmail.com>
2025-06-09 18:18:15 +0300

    Updates

---
+++ assets/about-DCg1lUV7.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-B2iyLJIv.js";const e=function(){return o.jsx("div",{className:"p-2",children:"Hello from About!"})};export{e as component};
--- assets/about-DhQTdQiR.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-C9th9T5l.js";const e=function(){return o.jsx("div",{className:"p-2",children:"Hello from About!"})};export{e as component};
