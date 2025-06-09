dccec6178d91e23be7fcf776987c741161a6913e
Deniz Satır <deniznehlyadyuk@gmail.com>
2025-06-09 17:47:44 +0300

    Updates

--- assets/about-6amK-nCM.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-DTMFceKV.js";const e=function(){return o.jsx("div",{className:"p-2",children:"Hello from About!"})};export{e as component};
--- assets/index-DgQhccAF.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-DTMFceKV.js";const t=function(){return e.jsx("div",{className:"p-2",children:e.jsx("h3",{children:"Welcome Home!"})})};export{t as component};
