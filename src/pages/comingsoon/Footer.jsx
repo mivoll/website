a283a64dd4535e6855c0eda7078c50006b99d551
savolla <savolla@protonmail.com>
2025-06-10 18:20:16 +0300

    feature: create footer

--- src/pages/comingsoon/Footer.jsx
+++ src/pages/comingsoon/Footer.jsx
@@ -1,7 +1,8 @@
 export default function Footer() {
   return (
-   <div>
-     <p>footer</p>
-   </div>
-  )
+    <div className="h-full mt-40 text-center">
+      <div className="border border-[#48484d] h-0 border-b-0 border-l-0 border-r-0"></div>
+      <p className="text-[#a5a5a7] mt-30"> © 2025 Mivoll Dijital. Tüm hakları saklıdır. </p>
+    </div>
+  );
 }
